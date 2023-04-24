e1e2cd9a8eeddbf4c06463de46463200d87a178a
penetzdorfer-L <109728327+penetzdorfer-L@users.noreply.github.com>
2023-04-25 01:53:53 +0200

    worked on cleancode

--- java-kotlin-katas/src/main/katas/lupe/codewars/katas/kotlin/vowelCount/VowelCount.kt
+++ java-kotlin-katas/src/main/katas/lupe/codewars/katas/kotlin/vowelCount/VowelCount.kt
@@ -3,7 +3,7 @@ package lupe.codewars.katas.kotlin.vowelCount
 class VowelCount {
     fun getCount(str: String): Int {
         return str.lowercase().split("").stream()
-            .filter {it.equals("a") || it.equals("e") || it.equals("i") || it.equals("o") || it.equals("u") }
+            .filter { it.equals("a") || it.equals("e") || it.equals("i") || it.equals("o") || it.equals("u") }
             .count().toInt()
     }
 }
