39da9ba18711b787f776d7a7f988fadfda25fe4a
penetzdorfer-L <lukas.penetzdorfer@gmail.com>
2023-08-29 11:13:07 +0200

    add test for IpCounting java

--- java-kotlin-katas/src/test/katas/lupe/codewars/katas/java/coutIpAddresses/CountIpAddressesTest.java
+++ java-kotlin-katas/src/test/katas/lupe/codewars/katas/java/coutIpAddresses/CountIpAddressesTest.java
@@ -1,17 +1,22 @@
 package lupe.codewars.katas.java.coutIpAddresses;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CountIpAddressesTest {
-
     private final CountIpAddresses countIpAddresses = new CountIpAddresses();
-
-    @Test
-    public void testIPcounting() {
-        assertEquals( 50, countIpAddresses.ipsBetween( "10.0.0.0", "10.0.0.50" ) );
-        assertEquals( 246, countIpAddresses.ipsBetween( "20.0.0.10", "20.0.1.0" ) );
-        assertEquals( (1l << 32l) - 1l, countIpAddresses.ipsBetween( "0.0.0.0", "255.255.255.255" ) );
+    @ParameterizedTest
+    @CsvSource({
+            "10.0.0.0, 10.0.0.50, 50",
+            "20.0.0.10, 20.0.1.0, 246",
+            "0.0.0.0, 255.255.255.255, 4294967295",
+            "1.1.1.1, 1.1.1.1, 0",
+            "1.1.1.1, 1.1.1.2, 1"
+    })
+    public void testIpCounting(String start, String end, long expected) {
+        long result = countIpAddresses.ipsBetween(start, end);
+        assertEquals(result, expected);
     }
 }
